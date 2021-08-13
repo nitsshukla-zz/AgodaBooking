f57212773d866c3b69d26303a28d85b38128ed60
Nitin <nitin1.shukla@ril.com>
2021-08-13 19:43:41 +0530

    Initial commit
    onboarded Spring-boot

---
+++ src/main/java/com/agoda/booking/tracker/AgodaBookingsTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.agoda.booking.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgodaBookingsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgodaBookingsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agoda/booking/tracker/AgodaBookingsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agoda.booking.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgodaBookingsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
